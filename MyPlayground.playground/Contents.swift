fe573792d1a3a7d723191231ef10b1c6b6e010f9
Luizgustavo358 <luiz.gustavo358@gmail.com>
2025-07-09 19:55:51 -0300

    update

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,28 @@
-var numbers = [45, 73, 195, 53]
 
-var computedNumbers = [numbers[0]*numbers[1], numbers[1]*numbers[2], numbers[2]*numbers[3]]
+//Don't change this
+//var studentsAndScores = ["Amy": Int(readLine()!)!, "James": Int(readLine()!)!, "Helen": Int(readLine()!)!]
 
-print(computedNumbers)
+func highestScore(scores: [String: Int]) {
+  
+  //Write your code here.
+    var highestScore: Int = 0
+    
+    for student in scores {
+        if student.value > highestScore && student.value != nil {
+            highestScore = student.value
+        }
+    }
+    
+    print(highestScore)
+  
+}
+
+//Try some different scores.
+//Dont add the lines below to udemy!
+highestScore(
+    scores: [
+        "Amy": 88,
+        "James": 55,
+        "Helen": 99
+    ]
+)
