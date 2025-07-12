67100cfc13746fec22f77944f28b888e101640d5
Luizgustavo358 <luiz.gustavo358@gmail.com>
2025-07-12 17:37:01 -0300

    changing the background color in the buttons, putting a timer and implementing the progressbar

--- apps/Quizzler/Quizzler-iOS13/ViewController.swift
+++ apps/Quizzler/Quizzler-iOS13/ViewController.swift
@@ -44,9 +44,9 @@ class ViewController: UIViewController {
         let actualAnswer = quiz[questionNumber].answer
         
         if userAnswer == actualAnswer {
-            print("Correct!")
+            sender.backgroundColor = UIColor.green
         } else {
-            print("Incorrect!")
+            sender.backgroundColor = UIColor.red
         }
         
         if questionNumber < quiz.count - 1 {
@@ -55,11 +55,14 @@ class ViewController: UIViewController {
             questionNumber = 0
         }
         
-        updateUI()
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
     }
     
-    func updateUI() {
+    @objc func updateUI() {
         questionLabel.text = quiz[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
     }
 }
 
