5ddce808761c82fcde74846afcad6aa4702370c6
Luizgustavo358 <luiz.gustavo358@gmail.com>
2025-08-25 21:12:38 -0300

    Added ByteCoin app

---
+++ apps/ByteCoin/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,12 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Luiz Gustavo Bragança dos Santos on 25/08/25.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
