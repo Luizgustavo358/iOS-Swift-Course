a75bdadb5f9a87cdd86dea579385b791e72ead2d
Luizgustavo358 <luiz.gustavo358@gmail.com>
2025-08-25 23:44:02 -0300

    creating the chat app

---
+++ apps/Flash-Chat/Flash-Chat/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flash-Chat
+//
+//  Created by Luiz Gustavo Bragança dos Santos on 25/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
