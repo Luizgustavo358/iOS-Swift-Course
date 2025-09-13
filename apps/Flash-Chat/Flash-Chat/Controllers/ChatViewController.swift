997daf5457689af7f6ca35184785a704a8b9b0ed
Luizgustavo358 <luiz.gustavo358@gmail.com>
2025-09-13 17:08:36 -0300

    logging out users with firebase

--- apps/Flash-Chat/Flash-Chat/Controllers/ChatViewController.swift
+++ apps/Flash-Chat/Flash-Chat/Controllers/ChatViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 class ChatViewController: UIViewController {
     
@@ -14,9 +17,19 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "⚡️FlashChat"
+        navigationItem.hidesBackButton = true
     }
     
-    
     @IBAction func sendPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
     }
 }
