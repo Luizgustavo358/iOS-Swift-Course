07d15fb253385dd2884429eeb8ed384013e90c4b
Luizgustavo358 <luiz.gustavo358@gmail.com>
2025-09-23 22:14:31 -0300

    creating the layout of the dicee app

---
+++ apps/Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Luiz Gustavo Bragança dos Santos on 23/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea(.all)
+            
+            VStack {
+                Image("diceeLogo")
+                
+                Spacer()
+                
+                HStack {
+                    DiceView(n: 1)
+                    DiceView(n: 2)
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundStyle(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
