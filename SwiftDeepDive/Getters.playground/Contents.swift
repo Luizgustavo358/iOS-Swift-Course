65e0ce11bac1cd8d1eb14db2e8442e004338eae5
Luizgustavo358 <luiz.gustavo358@gmail.com>
2026-01-01 18:12:46 -0300

    computed property

---
+++ SwiftDeepDive/Getters.playground/Contents.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+var pizzaInInches: Int = 10 {
+    willSet {
+    }
+    didSet {
+        if pizzaInInches >= 18 {
+            print("Invalid size specified, pizzaInInches set to 18.")
+            pizzaInInches = 18
+        }
+    }
+}
+
+pizzaInInches = 38
+print(pizzaInInches)
+
+var numberOfPeople: Int = 12
+let slicesPerPerson: Int = 4
+
+var numberOfSlices: Int {
+    get {
+        return pizzaInInches - 4
+    }
+    set {
+        print("numberOfSlices now has a new value which is \(newValue)")
+    }
+}
+
+var numberOfPizza: Int {
+    get {
+        let numberOfPeopleFedPerPizza = numberOfSlices / slicesPerPerson
+        
+        return numberOfPeople / numberOfPeopleFedPerPizza
+    }
+    set {
+        let totalSlices = numberOfSlices * newValue
+        numberOfPeople = totalSlices / slicesPerPerson
+    }
+}
+
+
