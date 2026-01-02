8903b6bbc625d8cb2651ae3f338949ae0b966664
Luizgustavo358 <luiz.gustavo358@gmail.com>
2026-01-02 00:00:10 -0300

    initializing the calculator app

---
+++ apps/Calculator-Advanced/Calculator-Advanced/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Calculator-Advanced
+//
+//  Created by Luiz Gustavo Bragança dos Santos on 01/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        // What should happen when a non-number button is pressed
+    }
+    
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        // What should happen when a number is entered into the keypad
+    }
+}
+
