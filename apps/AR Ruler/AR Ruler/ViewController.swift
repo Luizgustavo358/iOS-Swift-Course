6155cedf59aad2861dcf273e7dd2b71576242066
Luizgustavo358 <luiz.gustavo358@gmail.com>
2026-01-26 18:57:18 -0300

    finishing the AR Ruler app

--- apps/AR Ruler/AR Ruler/ViewController.swift
+++ apps/AR Ruler/AR Ruler/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet var sceneView: ARSCNView!
     
     var dotNodes = [SCNNode]()
+    var textNode = SCNNode()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,6 +43,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if dotNodes.count >= 2 {
+            for dot in dotNodes {
+                dot.removeFromParentNode()
+            }
+            
+            dotNodes = [SCNNode]()
+        }
+        
         if let touchLocation = touches.first?.location(in: sceneView) {
             let hitTestResults = sceneView.hitTest(
                 touchLocation,
@@ -89,7 +98,32 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // distance = √((x2 - x1)² + (y2 - y1)² + (z2 - z1)²)
         let distance = sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2))
+        let result = String(format: "%.2f", abs(distance) * 100)
         
-        print(abs(distance))
+        updateText(text: "\(result) cm", atPosition: end.position)
+    }
+    
+    func updateText(text: String, atPosition position: SCNVector3) {
+        textNode.removeFromParentNode()
+        
+        let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+        
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.red
+        
+        textNode = SCNNode(geometry: textGeometry)
+        
+        textNode.position = SCNVector3(
+            x: position.x,
+            y: position.y + 0.01,
+            z: position.z
+        )
+        
+        textNode.scale = SCNVector3(
+            x: 0.01,
+            y: 0.01,
+            z: 0.01
+        )
+        
+        sceneView.scene.rootNode.addChildNode(textNode)
     }
 }
