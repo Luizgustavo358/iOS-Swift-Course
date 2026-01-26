d10963d9b912fd0b56444aa45a4ea3ee7f64b31c
Luizgustavo358 <luiz.gustavo358@gmail.com>
2026-01-26 18:36:54 -0300

    calculating the distance between 2 dots

--- apps/AR Ruler/AR Ruler/ViewController.swift
+++ apps/AR Ruler/AR Ruler/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var dotNodes = [SCNNode]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -69,5 +71,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         )
         
         sceneView.scene.rootNode.addChildNode(dotNode)
+        
+        dotNodes.append(dotNode)
+        
+        if dotNodes.count >= 2 {
+            calculate()
+        }
+    }
+    
+    func calculate() {
+        let start = dotNodes[0]
+        let end = dotNodes[1]
+        
+        let a = end.position.x - start.position.x
+        let b = end.position.y - start.position.y
+        let c = end.position.z - start.position.z
+        
+        // distance = √((x2 - x1)² + (y2 - y1)² + (z2 - z1)²)
+        let distance = sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2))
+        
+        print(abs(distance))
     }
 }
